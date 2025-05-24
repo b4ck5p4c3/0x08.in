36f4b7c723b1f5cb2b2e7dcabf42f18b148f0516
heeeyflo <heeeyflo@users.noreply.github.com>
2025-05-24 13:02:14 +0000

    deploy: 7cdf4ed89c4dd92940e10b6fa8bbd1a0d0de0493

---
+++ _next/static/chunks/pages/_app-594b956134c217b2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{1018:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(7929)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(1018),n(7867))),_N_E=_.O()}]);
