936ea7d456665bca9205a8c374be2a1cb4535dbb
Dmitry Nourell <hey@flo.boo>
2025-05-25 15:38:31 +0300

    feat: fix migration issues with Next canary

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,12 @@
 import type { Config } from 'tailwindcss'
-
-import fluid, { extract, fontSize, screens } from 'fluid-tailwind'
-
 export default {
   content: {
-    extract,
     files: [
       './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
       './src/components/**/*.{js,ts,jsx,tsx,mdx}',
       './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
   },
-  plugins: [
-    fluid
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
-    },
-    fontSize,
-    screens,
-  },
+  plugins: [],
+  theme: {},
 } satisfies Config
